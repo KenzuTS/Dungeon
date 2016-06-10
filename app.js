02eb58a330012be7af6b62499fc44a7e8874fbcc
Tonneau Sébastien <TonneauS@gmail.com>
2016-06-10 09:27:13 +0200

    init project

---
+++ app.js
@@ -0,0 +1,21 @@
+var game = new Phaser.Game(
+    800, 
+    600,
+    Phaser.CANVAS, 
+    "phaser-example", 
+    {   preload: preload, 
+        create: create, 
+        update: update, 
+        render: render });
+
+function preload() {    
+}
+
+function create() {
+}
+
+function update () {
+}
+
+function render () {
+}
