ce805cca4c3bf548fadb7ebef0bae6b620186d02
KenzuTS <TonneauS@gmail.com>
2016-06-14 12:37:57 +0200

    group ennemies + items et overlap items

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ var game = new Phaser.Game(
 function preload() {
 
     game.load.tilemap('map', 'assets/tilemaps/json/dungeonTest.json', null, Phaser.Tilemap.TILED_JSON);
+
     game.load.image('Cave', 'assets/roguelike-cave-pack/Spritesheet/roguelikeDungeon_transparent.png');
     game.load.image('Rogue', 'assets/roguelike-pack/Spritesheet/roguelikeSheet_transparent.png');
+    game.load.image('key', 'assets/DawnLike/Items/Key.png');
+
 	game.load.spritesheet('characters', 'assets/Characters/characters.png', 16, 16);
 }
 
@@ -21,9 +24,12 @@ var map, layer, ground;
 var player;
 var skeleton;
 var cursors;
+var ennemiesGroup, itemsGroup;
 
 function create() {
 
+
+    /* MAP */
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     map = game.add.tilemap('map');
@@ -45,22 +51,40 @@ function create() {
 
     map.addTilesetImage('Rogue');
 
+    /* PLAYER */
     player = new Player(game.world.centerX, game.world.centerY, 1);
 
+    /* CAMERA */
     game.camera.follow(player.sprite);
 
+    /* INPUTS */
     cursors = game.input.keyboard.createCursorKeys();
+
+    /* GROUPS */
+    ennemiesGroup = game.add.group();
+    skeleton = new Skeleton(game.world.centerX - 16, game.world.centerY - 16, 1);
+    
+    ennemiesGroup.add(skeleton.sprite);
+
+    itemsGroup = game.add.group();
 }
 
 function update() {
 
     game.physics.arcade.collide(player.sprite, layer);
+    game.physics.arcade.overlap(player.sprite, itemsGroup, collisionHandler, null, this);
 
     //player.move();
-    player.moveVelocity()
+    player.moveVelocity();
 }
 
 function render() {
 
     game.debug.body(player.sprite);
+}
+
+function collisionHandler(player, item){
+
+    console.log(item)
+    item.kill();
 }
--- ennemies/Skeleton.js
+++ ennemies/Skeleton.js
@@ -3,10 +3,6 @@ function Skeleton(x, y, scale){
 	this.y = y;
 	this.scale = scale;
 	this.sprite = null;
-	this.tween;
-	this.distX = x;
-	this.distY = y;
-	this.isWalking = false;
 
 	this.Awake = function(){
 
@@ -23,81 +19,14 @@ function Skeleton(x, y, scale){
 	    this.sprite.animations.add('up', [45,46,47], 6, true);
 
 	    this.sprite.play('down');
-	}	
+	}
 
-    this.move = function(){
-	
-		if (!this.isWalking) {
+	this.kill = function(){
 
-	    	if (cursors.down.isDown)
-	    	{
-	    		this.isWalking = true;
-	    	    this.sprite.play('down');
-	    	    this.distY = this.sprite.position.y + 16 * this.scale;
-	    	    this.tween = game.add.tween(this.sprite).to( { x: this.distX, y: this.distY }, 300);
-	    		this.tween.start();
-	    	}
-		
-	    	else if (cursors.left.isDown)
-	    	{
-	    		this.isWalking = true;
-	    	    this.sprite.play('left');
-	    	    this.distX = this.sprite.position.x - 16 * this.scale;
-	    	    this.tween = game.add.tween(this.sprite).to( { x: this.distX, y: this.distY }, 300);
-	    		this.tween.start();
-	    	}
-		
-	    	else if (cursors.right.isDown)
-	    	{
-	    		this.isWalking = true;
-	    	    this.sprite.play('right');
-	    	    this.distX = this.sprite.position.x + 16 * this.scale;
-	    	    this.tween = game.add.tween(this.sprite).to( { x: this.distX, y: this.distY }, 300);
-	    		this.tween.start();
-	    	}
-		
-	    	else if (cursors.up.isDown)
-	    	{
-	    		this.isWalking = true;
-	    	    this.sprite.play('up');
-	    	    this.distY = this.sprite.position.y - 16 * this.scale;
-	    	    this.tween = game.add.tween(this.sprite).to( { x: this.distX, y: this.distY }, 300);
-	    		this.tween.start();
-	    	}
-		}
-	
-		// stop animations
-    	if (this.sprite.position.x == this.distX && this.sprite.position.y == this.distY)
-    	{
-    	    this.isWalking = false;
-
-    	    if (cursors.down.isUp && cursors.left.isUp && cursors.right.isUp && cursors.up.isUp) {
-
-    	    	switch(this.sprite.animations.currentAnim.name){
-		
-    	    	    case "down":
-    	    	        this.sprite.animations.stop();
-    	    	        this.sprite.frame = 4;
-    	    	    break;
-		
-    	    	    case "left":
-    	    	        this.sprite.animations.stop();
-    	    	        this.sprite.frame = 16;
-    	    	    break;
-		
-    	    	    case "right":
-    	    	        this.sprite.animations.stop();
-    	    	        this.sprite.frame = 28;
-    	    	    break;
-		
-    	    	    case "up":
-    	    	        this.sprite.animations.stop();
-    	    	        this.sprite.frame = 40;
-    	    	    break;
-    	    	}
-    	    }
-    	}
-    }
+		var key = new Key(this.sprite.position.x, this.sprite.position.y)
+		itemsGroup.add(key.sprite);
+		this.sprite.kill();
+	}
 
     this.Awake();
 }
