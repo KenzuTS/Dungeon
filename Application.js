8f84a492bd62e5f56f3d7c341f92221c2da398a7
falnestu <bastleroy@gmail.com>
2016-06-15 16:13:33 +0200

    add Application.js

---
+++ Application.js
@@ -0,0 +1,27 @@
+var Application = {
+	Canvas : {
+		WIDTH : 800,
+		HEIGHT : 600
+	},
+	WORLD_SIZE : {
+		WIDTH : 1600,
+		HEIGHT : 1600
+	},
+	TILE_SIZE : 16,
+	Player : {
+		Frame : 
+		{
+			DOWN : 4,
+			LEFT : 16,
+			RIGHT : 28,
+			UP : 40
+		},
+		VELOCITY : 7500
+	},
+	
+	Time : {
+		FADE_DEATH : 1000,
+		TWEEN_ATTACK : 30
+	},
+	SCALE : 2
+}
