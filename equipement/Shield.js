1bbe46f8064d896b2997093c7eb8201daa106c7b
falnestu <bastleroy@gmail.com>
2016-06-23 16:33:51 +0200

    finish inventory

--- equipement/Shield.js
+++ equipement/Shield.js
@@ -1,6 +1,5 @@
-function Shield(attack, defense, maxDurability){
-
-	Equipement.call(this, attack, defense, maxDurability);
+function Shield(x, y, attack, defense, maxDurability){
+	Equipement.call(this, attack, defense, maxDurability, x, y , 'shield');
 }
 
 Shield.prototype = Object.create(Equipement.prototype);
